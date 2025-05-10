a75dc6fa22bdea085183c28fc0853e93df0152bf
louisraymond <39345994+louisraymond@users.noreply.github.com>
2025-05-10 16:32:23 +0200

    Time: 26 ms (17.27%), Space: 59.4 MB (15.15%) - LeetHub

--- LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas.js
+++ LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -4,26 +4,17 @@
  * @return {number}
  */
 
-let countSessions = (piles, k) => {
-    let sessions = 0
-    for (i = 0; i < piles.length; i++) {
-        sessions += Math.ceil(piles[i] / k)
-    }
-
-    return sessions
-}
 
 var minEatingSpeed = function (piles, h) {
     piles.sort((a, b) => a - b)
-
     let left = 1, right = piles[piles.length - 1]
+
+    let sessions = (k) => piles.reduce((acc, val) => acc + Math.ceil(val / k), 0)
 
     while (left < right) {
         let mid = (left + right) >> 1
-        let result = countSessions(piles, mid)
-
-        result <= h ? right = mid  : left = mid + 1
+        sessions(mid) <= h ? right = mid : left = mid + 1
     }
 
-    return right
+    return left
 };
