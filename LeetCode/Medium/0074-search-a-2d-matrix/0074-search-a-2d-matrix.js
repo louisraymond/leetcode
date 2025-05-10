55143c24fed868219037101b3ee39ae4fac039ca
louisraymond <39345994+louisraymond@users.noreply.github.com>
2025-05-10 19:26:35 +0200

    Time: 0 ms (100%), Space: 53.3 MB (80.72%) - LeetHub

---
+++ LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+
+    let findRow = () => {
+        let left = 0, right = matrix.length - 1
+        while (left <= right) {
+            let mid = (left + right) >> 1
+            matrix[mid][0] <= target ? left = mid + 1 : right = mid - 1
+        }
+        return matrix[right] || []
+    }
+
+    let findTarget = (row) => {
+        let left = 0, right = row.length - 1
+        while (left <= right) {
+            let mid = (left + right) >> 1
+            if (row[mid] === target) return true
+            row[mid] <= target ? left = mid + 1 : right = mid - 1
+        }
+        return false
+    }
+
+    row = findRow()
+
+    return findTarget(row)
+};
