7c5a2a2b25be5550fc7611980d948b1acba66f25
louisraymond <39345994+louisraymond@users.noreply.github.com>
2025-05-11 16:09:21 +0200

    Time: 136 ms (10.31%), Space: 78.9 MB (19.91%) - LeetHub

---
+++ LeetCode/Medium/1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function (root) {
+    let currentRequirement = root.val, counter = 0
+
+    let dfs = (node,requirement) => {
+        if (!node) return 0
+
+        if (node.val >= requirement) {
+            counter++
+            requirement = node.val
+        }
+
+        if (node.left) dfs(node.left,requirement)
+        if (node.right) dfs(node.right, requirement)
+    }
+
+    dfs(root,currentRequirement)
+    return counter
+};
