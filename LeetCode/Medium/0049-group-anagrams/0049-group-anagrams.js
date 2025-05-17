aa357a1af589baa12bfc4b935c703d28e43f9b4e
louisraymond <39345994+louisraymond@users.noreply.github.com>
2025-05-17 17:52:22 +0200

    Time: 30 ms (69.21%), Space: 67.6 MB (59.93%) - LeetHub

---
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map = {}
+    for ( let word of strs) {
+        copy = word.split("").sort().join("")
+        if (map[copy]) {
+            map[copy].push(word)
+        }else {
+            map[copy] = [word]
+        }
+    }
+    return Object.values(map)
+};
