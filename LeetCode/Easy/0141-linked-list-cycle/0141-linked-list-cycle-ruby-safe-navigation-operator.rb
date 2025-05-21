164865e26f5113bc3261fdc54d94cb7fcac62ae3
louisraymond <39345994+louisraymond@users.noreply.github.com>
2025-05-21 12:56:28 +0200

    Time: 66 ms (79.92%), Space: 212.3 MB (48.11%) - LeetHub

---
+++ LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle-ruby-safe-navigation-operator.rb
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @return {Boolean}
+def hasCycle(head)
+  tortoise = head
+  hare = head&.next
+
+  while hare && hare.next
+    return true if tortoise == hare
+    tortoise = tortoise.next
+    hare = hare.next.next
+  end
+
+  false
+end
