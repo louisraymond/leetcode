b6b38635a996283f6d5cc53add79b8429f148de1
louisraymond <39345994+louisraymond@users.noreply.github.com>
2025-05-21 14:04:20 +0200

    Time: 4 ms (31.63%), Space: 212.2 MB (10.2%) - LeetHub

---
+++ LeetCode/Medium/0046-permutations/0046-permutations-ruby.rb
@@ -0,0 +1,18 @@
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute(nums)
+    return [[]] if nums.empty?
+
+    permutations = permute(nums[1..])
+    results = []
+
+    permutations.each do |p|
+        (0..p.length).each do |i|
+            copy = p.dup
+            copy.insert(i,nums[0])
+            results << copy
+        end
+    end
+
+    return results
+end
