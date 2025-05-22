d0b662e390261fe44f523cb234731cf6b319f48e
louisraymond <39345994+louisraymond@users.noreply.github.com>
2025-05-22 22:58:16 +0200

    Time: 4 ms (91.45%), Space: 211.8 MB (10.54%) - LeetHub

---
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer-ruby.rb
@@ -0,0 +1,22 @@
+# @param {String} s
+# @return {Integer}
+def roman_to_int(s)
+    map = {
+    "I" => 1,
+    "V" => 5,
+    "X" => 10,
+    "L" => 50,
+    "C" => 100,
+    "D" => 500,
+    "M" => 1000
+    }
+    sum = 0
+    carry = 0
+    s.chars.each_with_index do |char, i|
+        curr_val = map[char]
+        next_val = map[s[i + 1]] if i + 1 < s.length
+        curr_val = next_val > curr_val ? -curr_val : curr_val if next_val
+        sum += curr_val
+    end
+sum
+end
