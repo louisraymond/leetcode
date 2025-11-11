5f8bdd4900bd71a7289e640e56e0fca468ce1685
louisraymond <39345994+louisraymond@users.noreply.github.com>
2025-11-11 12:45:09 +0000

    Time: 24 ms (24.25%) | Memory: 60.8 MB (32.79%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/meeting-rooms-ii.js
@@ -3,20 +3,17 @@
  * @return {number}
  */
 var minMeetingRooms = function (intervals) {
-    const endingHeap = new MinHeap()
-    endingHeap.insert(Infinity)
-    let rooms = 0, maxRooms = 0
+    const heap = new MinHeap()
+    let maxRooms = 0
 
     intervals.sort(([startA], [startB]) => startA - startB)
 
     for (const [start, end] of intervals) {
-        rooms++
-        endingHeap.insert(end)
-        while (start >= endingHeap.root()) {
-            rooms--
-            endingHeap.pop()
-        }
-        maxRooms = Math.max(rooms, maxRooms)
+        while (heap.size() && heap.root() <= start) { heap.pop() }
+        heap.insert(end)
+
+        maxRooms = Math.max(heap.size(), maxRooms)
     }
+
     return maxRooms
 };
