e82d88b9868c5f053512448802269ad63d8e52a0
louisraymond <39345994+louisraymond@users.noreply.github.com>
2025-12-11 15:39:16 +0100

    Time: 69 ms (11.95%) | Memory: 67.2 MB (92.16%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort((a, b) => a - b);
+    
+    for (i=0; i < nums.length-1; i++) if (nums[i]===nums[i+1])  return true
+    return false
+};
