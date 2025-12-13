690a6f3e3ad515dd2ffe5bc20e4ba51cba0d971f
louisraymond <39345994+louisraymond@users.noreply.github.com>
2025-12-13 16:18:37 +0100

    Time: 200 ms (55.18%) | Memory: 87.1 MB (82.85%) - LeetSync

---
+++ 883-car-fleet/car-fleet.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+var carFleet = function (target, position, speed) {
+    if (!position.length) return 0
+
+    let fleets = []
+
+    let compositeMap = position.map((_, i) => [position[i], speed[i]]).sort((a, b) => b[0] - a[0])
+    const eta = ([position, speed]) => (target - position) / speed
+
+    for (let i = 0; i < compositeMap.length; i++) {
+        let carEta = eta(compositeMap[i])
+        if (fleets.length !== 0 && fleets[fleets.length - 1] >= carEta) continue
+        fleets.push(carEta)
+    }
+
+    return fleets.length
+};
